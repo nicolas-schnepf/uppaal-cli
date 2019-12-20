4b282be64305d87000ebd3c67871cd82e69f60f1
marius@cs.aau.dk <marius@cs.aau.dk@8ec0fef4-c970-49c4-9cb4-19fa91ce8067>
2019-12-20 17:29:41 +0000

    added Uppaal and a CLI project prototype
    git-svn-id: https://svn01.ist.aau.dk/svn/cs/Fileshares/accessibility@3 8ec0fef4-c970-49c4-9cb4-19fa91ce8067

---
+++ CLI/src/org/uppaal/cli/Command.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.uppaal.cli;
+
+/**
+ * Command class parses an input line into a command and its arguments separated by white space.
+ * @author Marius Mikucionis <marius@cs.aau.dk>
+ */
+public class Command
+{
+    private String command = null;
+    private String args = null;
+    public Command(String line) {
+        int pos = 0;
+        // skip white space:
+        while (pos < line.length() && Character.isWhitespace(line.charAt(pos)))
+            ++pos;
+        int begin = pos;
+        // find white next space:
+        while (pos < line.length() && !Character.isWhitespace(line.charAt(pos)))
+            ++pos;
+        command = line.substring(begin,pos);
+        // skip white space:
+        while (pos < line.length() && Character.isWhitespace(line.charAt(pos)))
+            ++pos;        
+        args = line.substring(pos);
+    }
+    public String getCommand() {
+        return command;
+    }
+    public String getArgs(){
+        return args;
+    }
+}
