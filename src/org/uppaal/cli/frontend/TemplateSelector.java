c42de4668f366e729d645ab720fe1e89755099a5
nicolasschnepf <schnepf.nicolas@orange.fr>
2021-03-26 18:03:15 +0100

    commit all files of the frontend repository

--- src/org/uppaal/cli/frontend/TemplateSelector.java
+++ src/org/uppaal/cli/frontend/TemplateSelector.java
@@ -19,11 +19,14 @@ private ArrayList<Boolean> selected_templates;
 // hash set of selected templates
 private HashSet<String> templates;
 
+// number of lines of each template
+private ArrayList<Integer> lines;
 
 public TemplateSelector (LineReader reader, Context context) {
 	super(reader, context, 1);
 	this.selected_templates = new ArrayList<Boolean>();
 	this.templates = new HashSet<String>();
+	this.lines =new ArrayList<Integer>();
 }
 
 /**
@@ -40,12 +43,59 @@ public void clearSelectedTemplates() {
 	this.templates.clear();
 }
 
+/**
+* count the number of lines of the currently selected template
+* @return the number of lines of the current template
+*/
+
+public int countLines() {
+	String template = this.context.getTemplateExpert().showLoadedTemplate(this.current_element);
+	int lines = 0;
+	int pos = template.indexOf("\n");;
+	while (pos!=-1) {
+		pos = template.indexOf("\n", pos+1);
+		lines ++;
+	} this.lines.set(this.current_element, lines);
+
+	return lines;
+	}
 @Override
 public void setElementNumber() {
 	this.element_number = this.context.getTemplateExpert().getTemplateNumber();
 	this.current_element = 0;
-	for (int i=0;i<this.selected_templates.size();i++) this.selected_templates.set(i, false);
-	for (int j=this.selected_templates.size();j<this.element_number;j++) this.selected_templates.add(j, false);
+	this.lines.clear();
+	for (int i=0;i<this.selected_templates.size();i++) {
+		this.selected_templates.set(i, false);
+		this.lines.add(0);
+	}
+	for (int j=this.selected_templates.size();j<this.element_number;j++)  {
+		this.selected_templates.add(j, false);
+		this.lines.add(0);
+	}
+
+	int n = this.countLines();
+	int height = this.reader.getTerminal().getHeight();
+	if (n>height-5) this.view_number = n - (height - 5);
+	else this.view_number = 1;
+	this.current_view = 0;
+}
+
+@Override
+public void showNextElement() {
+	super.showNextElement();
+	if (this.lines.get(this.current_element)==0) this.countLines();
+	int height = this.reader.getTerminal().getHeight();
+	if (this.lines.get(this.current_element)<=height-5)this.view_number = 1;
+	else this.view_number = this.lines.get(this.current_element) - (height - 5);
+}
+
+@Override
+public void showPreviousElement() {
+	super.showPreviousElement();
+	if (this.lines.get(this.current_element)==0) this.countLines();
+	int height = this.reader.getTerminal().getHeight();
+	if (this.lines.get(this.current_element)<=height-5)this.view_number = 1;
+	else this.view_number = this.lines.get(this.current_element) - (height - 5);
 }
 
 @Override
@@ -83,6 +133,25 @@ public void validateSelection() {
 
 @Override
 public String showCurrentElement() {
-	return this.context.getTemplateExpert().showLoadedTemplate(this.current_element);
+
+// if there is enough place to display the current template simply return it
+
+	String template = this.context.getTemplateExpert().showLoadedTemplate(this.current_element);
+	int height = this.reader.getTerminal().getHeight();
+	if (this.lines.get(this.current_element)<=height-5)return template;
+
+// otherwise split the template in order to make it fit in the current window
+
+	int starting_pos = 0;
+	int pos = 0;
+	int n = 0;
+
+	while (n< this.current_view+height-5) {
+		if (n==this.current_view) starting_pos = pos;
+		pos = template.indexOf("\n", pos+1);
+		n ++;
+	}
+	if (pos==-1 || pos>=template.length()) return template.substring(starting_pos);
+	else return template.substring(starting_pos, pos);
 }
 }
